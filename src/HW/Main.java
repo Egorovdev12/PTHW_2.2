487b5594434ce3696701f5cdd439943a779870bf
ILYA EGOROV <egorovcf@gmail.com>
2021-10-07 16:52:03 +0500

    Commit #1

---
+++ src/HW/Main.java
@@ -0,0 +1,10 @@
+package HW;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BinOps bo = new BinOps();
+        System.out.println(bo.sum("111", "10"));
+        System.out.println(bo.mult("111", "10"));
+    }
+}
